1c0a3d5ce384eb7420ec4f76ddcc0b2b693a4c7a
VIVEK RAI <94076044+Vivekrai2098@users.noreply.github.com>
2023-10-20 22:36:34 +0530

    Create_3_year_backtesting.py

---
+++ 3 year backtesting.py
@@ -0,0 +1,29 @@
+import backtrader as bt
+import datetime
+
+# Create a custom strategy
+class MyStrategy(bt.Strategy):
+    params = (
+        ("sma_period", 20),
+    )
+
+    def __init__(self):
+        self.data_close = self.data.close
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.data, period=self.params.sma_period
+        )
+
+    def next(self):
+        if self.data_close[0] > self.sma[0]:
+            # Buy signal
+            self.buy()
+        elif self.data_close[0] < self.sma[0]:
+            # Sell signal
+            self.sell()
+
+# Create a cerebro engine
+cerebro = bt.Cerebro()
+
+# Add data feed for the last three years
+data = bt.feeds.YahooFinanceData(
+    dataname="
